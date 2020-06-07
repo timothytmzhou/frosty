ab9558741961250b52a377afd1e18fdd8025aba2
stackdynamic <timothy.tm.zhou@gmail.com>
2020-06-06 20:36:34 -0700

    move run_in_executor out of util

--- src/main.py
+++ src/main.py
@@ -10,17 +10,16 @@ async def process_message(message):
     msg_info = Message_Info(message)
     # Iterates through the commands dict of the form {Trigger: func -> Call}:
     for trigger, func in commands.copy().items():
-        if trigger.match(msg_info.content):
-            if msg_info.user_level >= trigger.access_level:
-                async with message.channel.typing():
-                    args = trigger.slice(msg_info.content)
-                    # If so, passes the slice into the corresponding function,
-                    #     and adds the returned Call object's invoke() method
-                    #     to the client loop.
-                    task = await wrap_sync(client.loop, func, msg_info, *args)
-                    if isinstance(task, Call):
-                        client.loop.create_task(task.invoke())
-                break
+        if trigger.match(msg_info.content) and msg_info.user_level >= trigger.access_level:
+            async with message.channel.typing():
+                args = trigger.slice(msg_info.content)
+                # If so, passes the slice into the corresponding function,
+                #     and adds the returned Call object's invoke() method
+                #     to the client loop.
+                task = await client.loop.run_in_executor(None, func, msg_info, *args)
+                if isinstance(task, Call):
+                    client.loop.create_task(task.invoke())
+            break
 
 
 @client.event
--- src/util.py
+++ src/util.py
@@ -1,9 +1,3 @@
-from concurrent.futures import ThreadPoolExecutor
-
-async def wrap_sync(loop, func, *args):
-    # Can set executor to None if a default has been set for loop
-    return await loop.run_in_executor(ThreadPoolExecutor(), func, *args)
-
 def format_table(data, headers):
     header_count = len(headers)
     column_lengths = tuple(
