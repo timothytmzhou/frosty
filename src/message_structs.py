133c4cefbc6d6d2a33aca1cc54d36801e52e2743
stackdynamic <timothy.tm.zhou@gmail.com>
2021-01-09 02:07:33 -0800

    Refactor message_structs with new decorator system

--- src/message_structs.py
+++ src/message_structs.py
@@ -1,66 +1,105 @@
-import re
-from io import StringIO
-from discord import File
+import discord
+from src.config import PARAMS
+from inspect import getfullargspec
+from docstring_parser import parse
+from discord_slash import SlashCommand
+from discord_slash import SlashCommandOptionType
+from discord_slash.utils import manage_commands
+
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, auto_register=True)
 
 
-class Message_Info:
-    def __init__(self, message, guild):
-        self.guild = guild
-        self.message = message
-        self.content = message.content
-        self.channel = message.channel
-        self.author = message.author
-        self.name = self.author.name
-        self.discriminator = self.author.discriminator
-        self.tag = "{0}#{1}".format(self.name, self.discriminator)
+class CommandDocstringParser:
+    """
+    Parses a docstring for a command.
+    """
+
+    def __init__(self, command):
+        """
+        :param command: the function to parse the info of
+        """
+        self.argspec = getfullargspec(command)
+        self.docstring = command.__doc__
+        self.name = command.__name__
+
+        parsed = parse(self.docstring)
+        self.description = parsed.short_description
+        self.params = self.parse_params(parsed.params)
+
+    def parse_params(self, params):
+        """
+        Parses the params of the docstring.
+
+        :param params: output of docstring_parser.parse.params
+        """
+        parsed = []
+        num_args = len(self.argspec.args)
+        num_defaults = len(self.argspec.defaults) if self.argspec.defaults is not None else 0
+        for param in params:
+            if not param.arg_name in self.argspec.args:
+                raise ValueError(
+                    "Found unknown param {0} in docstring for {1}".format(param.arg_name,
+                                                                          self.name))
+            elif self.argspec.kwonlyargs:
+                raise ValueError("Commands should not have keyword only arguments")
+
+            arg_index = self.argspec.args.index(param.arg_name)
+            if arg_index == 0:
+                continue
+            param_name = param.arg_name
+            description = param.description
+            option_type = SlashCommandOptionType[param.type_name.upper()]
+            required = num_args - arg_index > num_defaults
+            parsed.append((param_name, description, option_type, required))
+        return parsed
 
 
-class Call:
-    def __init__(self, task, args):
-        self.task = task
-        self.args = args
+def command(name=None):
+    """
+    The command decorator. Returns a decorator which fills out discord_slash options automatically
+    based on parsed docstrings.
 
-    async def invoke(self):
-        if self.task:
-            await self.task(*self.args)
+    :param name: the name of the command (function name if None)
+    """
 
-    @staticmethod
-    async def send(channel, text):
-        if len(text) > 2000:
-            await channel.send("message is longer than 2000 characters, writing to file")
-            await channel.send(file=File(StringIO(text), "output.txt"))
-        else:
-            await channel.send(text)
+    def wrapper(func):
+        parsed = CommandDocstringParser(func)
+        command_name = parsed.name if name is None else name
+        slash_command = slash.slash(name=command_name, guild_ids=[PARAMS["guild_id"]],
+                                    description=parsed.description,
+                                    options=[manage_commands.create_option(*args) for args in
+                                             parsed.params])
+        return slash_command(func)
 
-    @staticmethod
-    async def delete(msg):
-        await msg.delete()
-
-    @staticmethod
-    async def replace(msg, text):
-        channel = msg.channel
-        await Call.delete(msg)
-        await Call.send(channel, text)
+    return wrapper
 
 
-class Trigger:
-    def __init__(self, pattern, name=None, protected=True):
-        self.pattern = pattern
-        # replace \u with discriminator pattern
-        tags = r"((?:(?:<@[!&]?\d+>|.+?#\d{4}|@here|@everyone)(?:$|\s))+)"
-        self.unsequenced_pattern = pattern.replace(r"\u", tags)
-        if name is None:
-            # assume pattern in the form ^/name args if name is not specified
-            assert re.match(r"\^\/\w+( .*)?", self.pattern)
-            self.name = self.pattern.split()[0][1:]
-        else:
-            self.name = name
-        self.protected = protected
+def subcommand(base=None, name=None):
+    """
+    The subcommand decorator. Returns a decorator which fills out discord_slash options
+    automatically based on parsed docstrings. Subcommands should be named like base_name.
 
-    def match(self, text):
-        return re.match(self.unsequenced_pattern, text, re.DOTALL)
+    :param base: the name of the base command (first part of function name if None)
+    :param name: the name of the subcommand (second part of function name if None)
+    """
 
-    def slice(self, text):
-        return tuple(
-            g.strip() for g in self.match(text).groups() if g is not None
-        )
+    def wrapper(func):
+        parsed = CommandDocstringParser(func)
+        base_name = base
+        sub_name = name
+        if "_" in parsed.name:
+            a, b = parsed.name.split("_", limit=1)
+            if base is None:
+                base_name = a
+            if name is None:
+                sub_name = b
+
+        slash_command = slash.subcommand(base=base_name, name=sub_name,
+                                         guild_ids=[PARAMS["guild_id"]],
+                                         description=parsed.description,
+                                         options=[manage_commands.create_option(*args) for args in
+                                                  parsed.params])
+        return slash_command(func)
+
+    return wrapper
