7e00707888b92156c97f7cae40c1e733d07cd4a7
stackdynamic <timothy.tm.zhou@gmail.com>
2021-08-12 02:41:14 +0000

    fix wolfram alpha support

--- src/extensions/query.py
+++ src/extensions/query.py
@@ -11,12 +11,13 @@ async def ask(ctx, query):
 
     :param string query: input to Wolfram Alpha
     """
-    await ctx.send(content="hol up lemme check...")
+    await ctx.send("> {}".format(query))
+    msg = await ctx.send(content="hol up lemme check...")
     try:
         res = await client.loop.run_in_executor(None, waClient.query, query)
-        msg = "".join("```md\n{0}```".format(r.text) for r in res.results)
-        if not msg:
-            msg = "```no text results found```"
+        content = "".join("```md\n{}```".format(r.text) for r in res.results)
+        if not content:
+            content = "```no text results found```"
     except AttributeError:
-        msg = "```Wolfram Alpha doesn't understand your query```"
-    await ctx.edit(content=msg)
+        content = "```Wolfram Alpha doesn't understand your query```"
+    await msg.edit(content=content)
